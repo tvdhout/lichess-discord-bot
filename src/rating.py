284ce335fb37a7fd0133fa932fa2fd507510c2a8
Thijs van den Hout <thijsvandenhout@live.nl>
2020-05-06 13:12:57 +0000

    delegate rating command

--- src/rating.py
+++ src/rating.py
@@ -1,6 +1,79 @@
-import requests  # need to also pip install "requests[security]"
 from bs4 import BeautifulSoup
-import re
+import discord
 import numpy as np
+import re
 
-print('Hello world')
+
+async def all_ratings(message, response, name):
+    """
+    Show the ratings for each gamemode and the average rating over ['Bullet', 'Blitz', 'Rapid', 'Classical']
+    :param message: user's message to reply to
+    :param response: HTTP response from the GET request to lichess
+    :param name: lichess username for which to check the rating
+    :return: Show the embedded ratings
+    """
+    soup = BeautifulSoup(response.text, 'html.parser')
+    ratings = soup.find_all('rating')  # ratings are stored in rating tags
+
+    if len(ratings) < 4:  # need at least 4 ratings for the average (if not found, the username may be inactive)
+        await message.channel.send("I can't find any ratings for this user!")
+        return
+
+    embed = discord.Embed(title=f"{name}'s ratings",
+                          url=f'https://lichess.org/@/{name}',
+                          colour=0x00ffff
+                          )
+    embed.set_thumbnail(url='https://raw.githubusercontent.com/tvdhout/Lichess-discord-bot/master/media/lichesslogo.'
+                            'png')
+
+    ratings_list = list()
+    average_gamemodes = ['Bullet', 'Blitz', 'Rapid', 'Classical']
+    provisional = False
+    for rating_tag in ratings:
+        strong_tag = rating_tag.find('strong')
+        if strong_tag:  # valid rating tag with rating inside
+            gamemode = rating_tag.find_parent('a').find('h3').string
+            rating = strong_tag.string
+            n_games = rating_tag.find('span').string
+
+            if gamemode in average_gamemodes:  # count towards average
+                embed.add_field(name=gamemode, value=f'**{rating}** ({n_games})', inline=True)
+                if rating.endswith('?'):
+                    provisional = True
+                    rating = rating[:-1]
+                ratings_list.append(int(rating))
+            else:
+                embed.add_field(name=gamemode, value=f'{rating} ({n_games})', inline=True)
+
+    average_rating = np.mean(ratings_list)
+    embed.add_field(name='Average rating (Bullet, Blitz, Rapid, Classical)',
+                    value=f'**{average_rating}{"?" if provisional else ""}**', inline=False)
+
+    await message.channel.send(embed=embed)
+
+
+async def gamemode_rating(message, response, name, gamemode: str):
+    """
+    Show the rating of a given user in a particular gamemode
+    :param message: user's message to reply to
+    :param response: HTTP response from the GET request to lichess
+    :param name: lichess username for which to check the rating
+    :param gamemode: which rating to check
+    :return: send the embedded message, return nothing
+    """
+    soup = BeautifulSoup(response.text, 'html.parser')
+    h3 = soup.find('h3', text=re.compile(rf'^{gamemode}$', re.I))
+    if not h3:
+        message.channel.send(f"I can't find {name}'s {gamemode} rating")
+    rating = h3.parent.find('rating').find('strong').string
+    n_games = h3.parent.find('rating').find('span').string
+
+    embed = discord.Embed(title=f"{name}'s rating",
+                          url=f'https://lichess.org/@/{name}',
+                          colour=0x00ffff
+                          )
+    embed.set_thumbnail(url='https://raw.githubusercontent.com/tvdhout/Lichess-discord-bot/master/media/lichesslogo.'
+                            'png')
+    embed.add_field(name=h3.string, value=f'{rating} ({n_games})')
+
+    await message.channel.send(embed=embed)
