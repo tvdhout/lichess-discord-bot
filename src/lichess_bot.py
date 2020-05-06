f077bff6c8799e435deb1d606345b8f6a2708bf6
Thijs van den Hout <thijsvandenhout@live.nl>
2020-05-06 15:10:13 +0200

    two rating functions

--- src/lichess_bot.py
+++ src/lichess_bot.py
@@ -1,17 +1,31 @@
-import discord
 from discord.ext import commands
 import requests  # need to also pip install "requests[security]"
-from bs4 import BeautifulSoup
-import re
-import numpy as np
+from rating import *
 
 TOKEN = open('/etc/lichessbottoken.txt').read()
-client = commands.Bot(command_prefix='!')
+client = commands.Bot(command_prefix='l!')
 
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('Logged in as {0.user}'.format(client))
+
+
+@client.command()
+async def help():
+    """
+    Show bot help (commands, etc)
+    :return:
+    """
+    pass  # TODO: help command
+
+
+@client.command()
+async def commands():
+    """
+    Alias for help
+    """
+    await help()
 
 
 @client.command(pass_context=True)
@@ -40,52 +54,12 @@ async def rating(context):
         await message.channel.send("I can't find any ratings for this user!")
         return
 
-    soup = BeautifulSoup(response.text, 'html.parser')
-    ratings = soup.find_all('rating')
+    if len(contents) == 2:  # !rating [name/url]
+        await all_ratings(message, response, name)
+    elif len(contents) == 3:  # !rating [name/url] [gamemode]
+        gamemode = contents[2]
+        await gamemode_rating(message, response, name, gamemode)
 
-    if len(ratings) < 4:
-        await message.channel.send("I can't find any ratings for this user!")
-        return
-
-    embed = discord.Embed(title=f"{name}'s ratings",
-                          url=url,
-                          )
-    embed.set_thumbnail(url='https://images.prismic.io/lichess/5cfd2630-2a8f-4fa9-8f78-04c2d9f0e5fe_lichess-box-1024'
-                            '.png')
-
-    ratings_list = []
-    provisional = 0
-    for rating_tag in ratings[:4]:
-        gamemode = rating_tag.find_parent('a').find('h3').string
-        rating = rating_tag.find('strong').string
-        n_games = rating_tag.find('span').string
-        embed.add_field(name=gamemode, value=f'{rating} ({n_games})', inline=False)
-        if rating.endswith('?'):
-            provisional = 1
-            rating = rating[:-1]
-        ratings_list.append(int(rating))
-
-    average_rating = np.mean(ratings_list)
-    embed.add_field(name='Average rating', value=f'**{average_rating}{provisional*"?"}**', inline=False)
-
-    if average_rating < 1000:
-        embed.colour = 0x9b0046
-    elif average_rating < 1200:
-        embed.colour = 0x8841a7
-    elif average_rating < 1400:
-        embed.colour = 0x5d2377
-    elif average_rating < 1600:
-        embed.colour = 0x1b5281
-    elif average_rating < 1800:
-        embed.colour = 0x2b84d3
-    elif average_rating < 2000:
-        embed.colour = 0x22ffa3
-    elif average_rating < 2200:
-        embed.colour = 0x22ff11
-    else:
-        embed.colour = 0xdd6a1b
-
-    await message.channel.send(embed=embed)
 
 if __name__ == '__main__':
     client.run(TOKEN)
