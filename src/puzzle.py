0c74d8c42ce10cca634b8781bffc29122081b19f
Thijs van den Hout <thijsvandenhout@live.nl>
2020-05-08 15:31:10 +0200

    Get puzzle screenshots in an easier way

--- src/puzzle.py
+++ src/puzzle.py
@@ -10,6 +10,7 @@ import time
 from typing import Optional
 
 BASE_DIR = '/home/thijs/Lichess-discord-bot'
+answer = None
 
 
 async def show_puzzle(message: discord.message.Message, puzzle_id: Optional[str] = '') -> None:
@@ -22,27 +23,39 @@ async def show_puzzle(message: discord.message.Message, puzzle_id: Optional[str]
         # Get a puzzle image
         fox.get(f'https://lichess.org/training/{puzzle_id}')
 
-        element = fox.find_element_by_class_name('main-board')
-        location = element.location
-        size = element.size
+        if puzzle_id == '':
+            puzzle_id = fox.current_url.split('/')[-1]
+
+        board = fox.find_element_by_class_name('puzzle__board')
         time.sleep(.8)  # wait for the last move to play out
-        img = Image.open(BytesIO(base64.b64decode(fox.get_screenshot_as_base64())))  # take screenshot of page
+        im = Image.open(BytesIO(base64.b64decode(board.screenshot_as_base64)))  # take screenshot of page
+
+        im.save(f'{BASE_DIR}/media/puzzle.png')
+
+        # #Get answer
+        # solution_button = fox.find_element_by_class_name("view_solution")
+        # fox.execute_script("arguments[0].setAttribute('class','view_solution show')", solution_button)
+        # solution_event = fox.find_elements_by_class_name("button")[-1]
+        # print(solution_button)
+        # solution_event.click()
+        # time.sleep(.1)
+        # moves = fox.find_element_by_class_name('good')
+        # print(moves)
+
         fox.quit()  # quit the connection
     except selenium.common.exceptions.NoSuchElementException:
         await message.channel.send(f"I can't find a puzzle with puzzle id '{puzzle_id}'")
         fox.quit()
         return
 
-    # Crop image to only the board
-    left = location['x']
-    top = location['y']
-    right = location['x'] + size['width']
-    bottom = location['y'] + size['height']
-    im = img.crop((left, top, right, bottom))
-
     im.save(f'{BASE_DIR}/media/puzzle.png')
 
-    await message.channel.send("Solve this!")
-    with open(f'{BASE_DIR}/media/puzzle.png', 'rb') as f:
-        puzzle = discord.File(f)
-        await message.channel.send(file=puzzle)
+    embed = discord.Embed(title=f"Solve this! (puzzle {puzzle_id})",
+                          url=f'https://lichess.org/training/{puzzle_id}',
+                          colour=0x00ffff
+                          )
+
+    puzzle = discord.File(f'{BASE_DIR}/media/puzzle.png', filename="puzzle.png")
+    embed.set_image(url="attachment://puzzle.png")
+    embed.add_field(name="Answer with !answer", value="Use the standard algebraic notation, e.g. Qxb7")
+    await message.channel.send(file=puzzle, embed=embed)
