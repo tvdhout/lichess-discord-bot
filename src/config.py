623e3514b8b4893b5e3883c793474bc2abe0f705
Thijs van den Hout <thijsvandenhout@live.nl>
2020-05-10 15:53:44 +0200

    config to merge with release

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-PREFIX = '$'  # Command prefix
-TOKEN = open('/etc/lichessbottoken_dev.txt').read()  # Bot token
-BASE_DIR = '/home/thijs/Lichess-discord-bot-dev'
+PREFIX = '-'  # Command prefix
+TOKEN = open('/etc/lichessbottoken.txt').read()  # Bot token
+BASE_DIR = '/home/thijs/Lichess-discord-bot'
 # BASE_DIR = '/Users/thijs/Documents/Side_projects/Lichess-discord-bot'
