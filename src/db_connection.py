08c2f0f3f3621cef0c3d983ebde8424137b7d6c6
tvdhout <thijsvandenhout@live.nl>
2020-12-30 21:42:05 +0100

    Database connection decorator

---
+++ src/db_connection.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from discord.ext.commands import Context
+
+
+def connect(func):
+    async def connection_wrapper(context: Context, **kwargs):
+        try:  # Connect to database
+            connection = mysql.connector.connect(user='thijs',
+                                                 host='localhost',
+                                                 database='lichess')
+            cursor = connection.cursor(buffered=True)
+            try:
+                await func(context, cursor, **kwargs)
+                connection.commit()
+            except Exception as e:
+                print(repr(e))
+            finally:
+                cursor.close()
+                connection.disconnect()
+        except mysql.connector.Error:
+            await context.send("Oops! I can't connect to the puzzle database. This might be because I am working on "
+                               "the database. If the problem persists, please let me know by filing an issue at "
+                               "https://github.com/tvdhout/Lichess-discord-bot/issues or post in the bot support "
+                               "server: https://discord.gg/xCpCRsp")
+            print("Database connection error")
+
+    return connection_wrapper
