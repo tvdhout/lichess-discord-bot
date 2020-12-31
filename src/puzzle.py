68bba995ab01a91bc16539c027625f1051ac8424
tvdhout <thijsvandenhout@live.nl>
2020-12-31 13:07:13 +0100

    Remove channel_puzzle entry when done + checkmate = win

--- src/puzzle.py
+++ src/puzzle.py
@@ -72,15 +72,15 @@ async def show_puzzle(context: Context, cursor, puzzle_id: str = '') -> None:
                     value=f"Answer using SAN ({initial_move_san}) or UCI ({initial_move_uci}) notation\n"
                           f"Puzzle difficulty rating: ||**{rating}**||")
 
-    await context.send(file=puzzle, embed=embed)  # send puzzle
+    msg = await context.send(file=puzzle, embed=embed)  # send puzzle
 
     # Set current puzzle as active for this channel.
     channel_puzzle = ("INSERT INTO channel_puzzles "
-                      "(ChannelId, PuzzleId, Moves, FEN) "
-                      f"VALUES (%s, %s, %s, %s) "
+                      "(ChannelId, PuzzleId, Moves, FEN, MessageId) "
+                      f"VALUES (%s, %s, %s, %s, %s) "
                       "ON DUPLICATE KEY UPDATE PuzzleId = VALUES(PuzzleId), "
-                      "Moves = VALUES(Moves), FEN = VALUES(FEN)")
-    data_puzzle = (str(context.message.channel.id), str(puzzle_id), ' '.join(moves), str(fen))
+                      "Moves = VALUES(Moves), FEN = VALUES(FEN), MessageId = VALUES(MessageId)")
+    data_puzzle = (str(context.message.channel.id), str(puzzle_id), ' '.join(moves), str(fen), str(msg.id))
 
     cursor.execute(channel_puzzle, data_puzzle)
 
@@ -114,27 +114,18 @@ async def answer_puzzle(context: Context, cursor, answer: str) -> None:
     message - the command entered by the user, used as a context to know which channel to post the reply to
     answer - the move the user provided
     """
-    get_puzzle = f"SELECT puzzles.PuzzleId, Rating, channel_puzzles.Moves, channel_puzzles.FEN " \
+    # Fetch the active puzzle from the channel_puzzles table
+    get_puzzle = f"SELECT puzzles.PuzzleId, Rating, channel_puzzles.Moves, channel_puzzles.FEN, MessageId " \
                  f"FROM channel_puzzles LEFT JOIN puzzles ON channel_puzzles.PuzzleId = puzzles.PuzzleId " \
-                 f"WHERE ChannelId = {str(context.message.channel.id)};"
-    cursor.execute(get_puzzle)
+                 f"WHERE ChannelId = %s;"
+    cursor.execute(get_puzzle, (str(context.channel.id),))
 
     try:
-        puzzle_id, rating, moves, fen = cursor.fetchall()[0]
+        puzzle_id, rating, moves, fen, message_id = cursor.fetchall()[0]
         moves = moves.split()
     except IndexError:
         await context.send(f"There is no active puzzle in this channel! Check `{PREFIX}commands` for how to start a "
                            f"puzzle")
-        return
-
-    if len(moves) == 0:
-        embed = discord.Embed(title=f"No active puzzle!",
-                              colour=0x00ffff
-                              )
-        embed.add_field(name="Oops!",
-                        value="I'm sorry. I currently don't have the answers to a puzzle. Please try another "
-                              f"`{PREFIX}puzzle`")
-        await context.send(embed=embed)
         return
 
     embed = discord.Embed(title=f"Answering puzzle ID: {puzzle_id}",
@@ -149,42 +140,69 @@ async def answer_puzzle(context: Context, cursor, answer: str) -> None:
     correct_san = board.san(board.parse_uci(correct_uci))
     stripped_correct_san = re.sub(r'[|#+x]', '', correct_san.lower())
 
+    # If the given answer is any checkmating move, it is correct no matter what the puzzle move is.
+    def is_answer_mate(a: str, notation: str = 'san') -> bool:
+        try:
+            if notation == 'san':
+                board.push_san(a)
+            else:
+                board.push_uci(a)
+            if board.is_game_over():
+                return True
+            board.pop()
+            return False
+        except ValueError:
+            return False
+
+    if is_answer_mate(answer) or is_answer_mate(answer, notation='uci') or is_answer_mate(answer.capitalize()):
+        embed.add_field(name="Correct!", value=f"Yes! {spoiler + answer + spoiler} is checkmate. "
+                                               f"You completed the puzzle! (difficulty rating {rating})")
+        await context.send(embed=embed)
+
+        delete_puzzle = (f"DELETE FROM channel_puzzles "
+                         f"WHERE ChannelId = %s;")
+        cursor.execute(delete_puzzle, (str(context.channel.id),))
+        return
+
     if stripped_answer in [correct_uci, stripped_correct_san]:  # Correct answer
         if len(moves) == 1:  # Last step in puzzle
-            moves.pop(0)
-            board.push(board.parse_uci(correct_uci))
-
-            fen = board.fen()
-
             embed.add_field(name="Correct!", value=f"Yes! The best move was {spoiler + correct_san + spoiler}. "
                                                    f"You completed the puzzle! (difficulty rating {rating})")
+            await context.send(embed=embed)
+            delete_puzzle = (f"DELETE FROM channel_puzzles "
+                             f"WHERE ChannelId = %s;")
+            cursor.execute(delete_puzzle, (str(context.channel.id),))
+            return
         else:  # Not the last step in puzzle
+            # Apply the correct move + opponent's follow up to the chess.Board
             moves.pop(0)
             board.push(board.parse_uci(correct_uci))
 
             reply_uci = moves.pop(0)
             move = board.parse_uci(reply_uci)
-            reply_san = board.san(move)
-            board.push(board.parse_uci(reply_uci))
+            reply_san = board.san(move)  # To show the user
+            board.push(move)
 
-            fen = board.fen()
+            fen = board.fen()  # Get the FEN of the board with applied moves to use for the next answer
 
             embed.add_field(name="Correct!",
                             value=f"Yes! The best move was {spoiler + correct_san + spoiler}. The opponent "
                                   f"responded with {spoiler + reply_san + spoiler}, "
                                   f"now what's the best move?")
+            await context.send(embed=embed)
+
+            # Update the moves list and FEN in the channel_puzzles table
+            update_query = (f"UPDATE channel_puzzles "
+                            f"SET Moves = %s, FEN = %s "
+                            f"WHERE ChannelId = %s;")
+            update_data = (' '.join(moves), str(fen), str(context.message.channel.id))
+
+            cursor.execute(update_query, update_data)
     else:  # Incorrect answer
         embed.add_field(name="Wrong!", value=f"{answer} is not the best move. Try again using `{PREFIX}"
                                              f"answer` or get the answer with `{PREFIX}bestmove`")
 
-    await context.send(embed=embed)
-
-    update_query = (f"UPDATE channel_puzzles "
-                    f"SET Moves = %s, FEN = %s "
-                    f"WHERE ChannelId = %s;")
-    update_data = (' '.join(moves), str(fen), str(context.message.channel.id))
-
-    cursor.execute(update_query, update_data)
+        await context.send(embed=embed)
 
 
 @db_connection.connect
@@ -198,8 +216,8 @@ async def give_best_move(context: Context, cursor) -> None:
 
     get_puzzle = f"SELECT puzzles.PuzzleId, Rating, channel_puzzles.Moves, channel_puzzles.FEN " \
                  f"FROM channel_puzzles LEFT JOIN puzzles ON channel_puzzles.PuzzleId = puzzles.PuzzleId " \
-                 f"WHERE ChannelId = {str(context.message.channel.id)};"
-    cursor.execute(get_puzzle)
+                 f"WHERE ChannelId = %s;"
+    cursor.execute(get_puzzle, (str(context.channel.id),))
 
     try:
         puzzle_id, rating, moves, fen = cursor.fetchall()[0]
@@ -207,16 +225,6 @@ async def give_best_move(context: Context, cursor) -> None:
     except IndexError:
         await context.send(f"There is no active puzzle in this channel! Check `{PREFIX}commands` for how to start a "
                            f"puzzle")
-        return
-
-    if len(moves) == 0:  # No active puzzle
-        embed = discord.Embed(title=f"No active puzzle!",
-                              colour=0x00ffff
-                              )
-        embed.add_field(name="Oops!",
-                        value="I'm sorry. I currently don't have the answers to a puzzle. Please try another "
-                              f"`{PREFIX}puzzle`")
-        await context.send(embed=embed)
         return
 
     embed = discord.Embed(title=f"Answering puzzle ID: {puzzle_id}",
@@ -228,24 +236,27 @@ async def give_best_move(context: Context, cursor) -> None:
     move = board.parse_uci(moves.pop(0))
     san_move = board.san(move)
     board.push(move)
-    fen = board.fen()
 
-    if len(moves) > 1:
+    if len(moves) > 1:  # More steps to come
         follow_up = board.parse_uci(moves.pop(0))
-        follow_up_san = board.san(follow_up)
+        follow_up_san = board.san(follow_up)  # To show the user
         board.push(follow_up)
-        fen = board.fen()
+
         embed.add_field(name="Answer", value=f"The best move is ||{san_move}||. "
                                              f"The opponent responded with ||{follow_up_san}||, now what's the best"
                                              f" move?")
-    else:
+        await context.send(embed=embed)
+
+        fen = board.fen()  # Updated FEN with the move played, to use in the next step
+        update_query = (f"UPDATE channel_puzzles "
+                        f"SET Moves = %s, FEN = %s "
+                        f"WHERE ChannelId = %s;")
+        update_data = (' '.join(moves), str(fen), str(context.channel.id))
+        cursor.execute(update_query, update_data)
+    else:  # End of the puzzle
         embed.add_field(name="Answer", value=f"The best move is ||{san_move}||. That's the end of the puzzle! "
                                              f"(difficulty rating {rating})")
-
-    await context.send(embed=embed)
-
-    update_query = (f"UPDATE channel_puzzles "
-                    f"SET Moves = %s, FEN = %s "
-                    f"WHERE ChannelId = %s;")
-    update_data = (' '.join(moves), str(fen), str(context.message.channel.id))
-    cursor.execute(update_query, update_data)
+        await context.send(embed=embed)
+        delete_puzzle = (f"DELETE FROM channel_puzzles "
+                         f"WHERE ChannelId = %s;")
+        cursor.execute(delete_puzzle, (str(context.channel.id),))
